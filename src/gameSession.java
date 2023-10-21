e1319098a916bcb22a87b7a5acae485c2ea5440a
Kevin Bedoya <kbedoya88@Kevins-MacBook-Pro-3.local>
2023-10-21 18:43:27 -0400

    Changes to gameSession.java: incoporated the game loop/a template for the turn logic

--- src/gameSession.java
+++ src/gameSession.java
@@ -15,8 +15,21 @@ public class gameSession{
     for(int i = 1; i < playerCount; i++){
         Player player = new Player(i);
         players.add(player);
-
     }
+
+    while(true)
+    {
+        for(int i = 0; i < players.size(); i++)
+        {
+            Player current = players.get(i);
+            current.action(current); 
+        }
+
+        if(players.size() == 1) break; 
+    }
+        Player winner = players.get(0); 
+
+        System.out.println("The winner of the game: "+winner.getName());
 
  }
 
